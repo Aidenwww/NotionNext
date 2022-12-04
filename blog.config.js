55e3c2b2758174d492e75249b40b7fa75caf6f3d
Aidenwww <72078670+Aidenwww@users.noreply.github.com>
2022-12-04 10:03:06 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
-  AUTHOR: 'Aiden', // 作者
+  AUTHOR: 'Aidenwww', // 作者
   BIO: 'somewhat lucky', // 作者简介
   LINK: 'https://tangly1024.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
